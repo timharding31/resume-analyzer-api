161057cc08724bbe4eb222837cdcd37b6163c6d9
Tim Harding <timharding@Tims-MacBook-Pro.local>
2023-03-31 12:01:25 -0700

    Initial commit

---
+++ app/api/endpoints.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from starlette.responses import RedirectResponse
+
+
+# Init FastAPI router for API endpoints
+api_routes = APIRouter(prefix="/api")
+
+
+@api_routes.get('/')
+def redirect_to_docs():
+    """Redirect to API docs when at site root"""
+    return RedirectResponse('/redoc')
+
+
+@api_routes.get('/hello/{name}')
+async def get_hello(name: str = 'world'):
+    return {"message": "Hello " + name}
---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api.endpoints import api_routes
+
+
+def create_app():
+    # Initialize FastAPI app
+    app = FastAPI()
+
+    # Enable CORS via middleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_headers=['*'],
+        allow_methods=['*'],
+        allow_origins=['*'],
+    )
+
+    app.include_router(api_routes)
+
+    return app
+
+
+application = create_app()
