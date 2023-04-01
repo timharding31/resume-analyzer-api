5a30651efe336b9b0fedf869ac5191b072c0a0f9
Tim Harding <timharding@Tims-MacBook-Pro.local>
2023-04-01 14:18:06 -0700

    Add endpoint for receiving files from FE

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,17 +1,18 @@
-from fastapi import APIRouter
-from starlette.responses import RedirectResponse
+from fastapi import APIRouter, UploadFile, File, Depends
+from pydantic import BaseModel
 
 
 # Init FastAPI router for API endpoints
 api_routes = APIRouter(prefix="/api")
 
-
-@api_routes.get('/')
-def redirect_to_docs():
-    """Redirect to API docs when at site root"""
-    return RedirectResponse('/redoc')
-
-
 @api_routes.get('/hello/{name}')
 async def get_hello(name: str = 'world'):
     return {"message": "Hello " + name}
+
+class ParseRequest(BaseModel):
+    name: str
+
+@api_routes.post('/new')
+async def new_parse_request(request: ParseRequest = Depends(), file: UploadFile = File(...)):
+    name = request.dict().get('name')
+    return { "filename": file.filename + ', uploaded by ' + name }
